9b153051164bb832bdbe4e4b0b4c3f7c4ce350ef
Younes <younes.ghoniem@gmail.com>
2022-03-07 22:37:57 +0100

    file creation bug fix

--- python/main.py
+++ python/main.py
@@ -4,9 +4,9 @@ if __name__ == '__main__':
     res_list = []
     m = 10
     for i in range(1, 11):
-        for p in range(1, m + 1):
-            res_list.append(test(p, m))
         with open("res-" + str(m) + ".dat", "a") as file:
-            file.write(str(res_list))
+            for p in range(1, m + 1):
+                res = ' '.join(str(e) for e in test(p, m))
+                file.write(res+"\n")
         res_list.clear()
         m += 10
